6e78245136b82a71728537882267e61c2c98a3de
Srijita Ganguly <sriparnaganguly222@gmail.com>
2026-09-21 10:20:53 +0530

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,14 @@
+/* Write a c program to print 1 to 5 using while loop.   */
+
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+	printf ("1 to 5 using while loop -> \n");
+	while (i<= 5)
+	{
+		printf ("%d \n", i);
+		i++;
+	}
+	return 0;
+}
