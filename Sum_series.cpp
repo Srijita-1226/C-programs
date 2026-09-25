1acdcda137df86fa7543d7264edf398831fe273f
Srijita Ganguly <sriparnaganguly222@gmail.com>
2026-09-25 17:07:31 +0530

    Add C program for sum of odd factorial series
    Implement a C program to calculate the sum of the series of factorials for odd numbers up to n.

---
+++ Sum_series.cpp
@@ -0,0 +1,30 @@
+/* Write a c program to find the sum of the following series :- 1!+3!+5!...... up to n numbers.
+  1! =1
+  3! = 3*2*1 = 6
+  5! = 5*4*3*2*1 = 120 */
+
+#include <stdio.h>
+int main ()
+{
+	int n, i=1, c=1, a=1;
+	long int fact, sum = 0;
+	printf ("Enter the value = ");
+	scanf ("%d", &n);
+	printf ("The sum of the series = \n");
+	
+	while (c<=n)  // outer...if true
+	{
+		i = 1 ;
+		fact = 1;
+		while (i<=a)  // inner
+		{
+			fact = fact*i;
+			i++;
+		}
+		sum = sum+fact;
+		c++;
+		a = a+2;
+	}
+	
+	return 0;
+}              
